09b2b382b3ccf107cb71144cebfb11a7465a19ff
salsam22 <76152023+salsam22@users.noreply.github.com>
2022-02-04 13:29:56 +0100

    Creació del formulari de registre i primers pasos per al login

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,11 +6,15 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(fields={"username"}, message="There is already an account with this username")
  */
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     /**
      * @ORM\Id
@@ -131,5 +135,25 @@ class User
         $this->rol = $rol;
 
         return $this;
+    }
+
+    public function getRoles(): array
+    {
+        return [$this->getRol()];
+    }
+
+    public function getSalt()
+    {
+        return null;
+    }
+
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+    public function getUserIdentifier(): String
+    {
+        return $this->getUsername();
     }
 }

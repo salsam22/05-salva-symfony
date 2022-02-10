78b4f418548e8ce704faa58ba918eac93aeea362
salsam22 <76152023+salsam22@users.noreply.github.com>
2022-02-10 12:11:25 +0100

    Creacio del fitxer makefile i ajusts del select

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -11,10 +11,14 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/user')]
+/**
+ * @Route("/user")
+ */
 class UserController extends AbstractController
 {
-    #[Route('/', name: 'user_index', methods: ['GET'])]
+    /**
+     * @Route("/", name="user_index", methods={"GET"})
+     */
     public function index(UserRepository $userRepository): Response
     {
         return $this->render('user/index.html.twig', [
@@ -22,7 +26,9 @@ class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'user_new', methods: ['GET', 'POST'])]
+    /**
+     * @Route("/new", name="user_new", methods={"GET", "POST"})
+     */
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
@@ -42,7 +48,9 @@ class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'user_show', methods: ['GET'])]
+    /**
+     * @Route("/{id}", name="user_show", methods={"GET"})
+     */
     public function show(User $user): Response
     {
         return $this->render('user/show.html.twig', [
@@ -50,7 +58,9 @@ class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'user_edit', methods: ['GET', 'POST'])]
+    /**
+     * @Route("/{id}/edit", name="user_edit", methods={"GET", "POST"})
+     */
     public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(UserType::class, $user);
@@ -68,7 +78,9 @@ class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'user_delete', methods: ['POST'])]
+    /**
+     * @Route("/delete/{id}", name="user_delete", methods={"POST"})
+     */
     public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
